535ab1a5532ba4198ac21fc3461fdb89e1a34f1c
Clare Lindley <clare.lindley@gmail.com>
2016-05-27 12:52:44 +0100

    Added actual and expected values to this test to make it easier to read

--- test/users.js
+++ test/users.js
@@ -4,16 +4,21 @@ const server = require('../index.js');
 const code = require('code'); // assertion library to use with Lab
 
 lab.experiment('Users: ', function() {
+
   lab.test('Successfully find a given User',
     function(done){
-      var options = {method: 'GET', url:'/users/574822a66e27ecd916949768'};
+
+      const actual = '574822a66e27ecd916949768';
+      const expected = '574822a66e27ecd916949768';
+
+      var options = {method: 'GET', url:'/users/' + actual};
       server.inject(options, function(response){
 
         // check response is an object
         code.expect(response.result).to.be.an.object();
 
         // verify that the user has come back in the response
-        code.expect(response.result.id).to.equal('574822a66e27ecd916949768');
+        code.expect(response.result.id).to.equal(expected);
 
         // verify status code is 200
         code.expect(response.statusCode).to.equal(200);
