01e434abe71bb07738df6ea02af104fc0390aaa9
Clare Lindley <clare.lindley@gmail.com>
2016-05-29 20:54:53 +0100

    Refactored User handlers into a separete controller

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,1 +1,61 @@
+"use strict"
 
+const User = require('../model/user');
+const Boom = require('boom');
+
+function UserController(){};
+
+UserController.prototype.create = function(request, reply) {
+
+  var user = new User({
+    username: request.payload.username,
+    password: request.payload.password,
+    contact: request.payload.contact
+  });
+
+  user.save(function(err, user){
+    if(err){
+      if(err.code === 11000){ // MongoDB Duplicate Key Error
+        reply(Boom.badRequest('User already exists'));
+      }
+      else if(err.name == 'ValidationError') { // Note: Mongoose errors don't have error codes
+        reply(Boom.badRequest('Missing Username or Password'));
+      }
+      else {
+        throw err;
+      }
+    }
+    else {
+      reply(user);
+    }
+  });
+
+}
+
+UserController.prototype.read = function(request, reply) {
+
+  User.findById(request.params.id, function (err, user) {
+
+    if (err){
+      if(err.name == 'CastError'){ // Note: Mongoose errors don't have error codes
+        reply(Boom.badRequest('Invalid User Id'));
+      }
+      else {
+        throw err;
+      }
+    }
+    else {
+      if(user){
+        reply(user);
+      }
+      else {
+        reply(Boom.notFound());
+      }
+    }
+
+  });
+
+}
+
+const userController = new UserController();
+module.exports = userController;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,39 +1,13 @@
 "use strict";
 
-const User = require('../model/user');
-const Boom = require('boom');
+const userController = require('../controller/user');
 
 module.exports = function(){
   return [
     {
       method: 'POST',
       path: '/users',
-      handler: function createUser(request, reply) {
-
-        var user = new User({
-          username: request.payload.username,
-          password: request.payload.password,
-          contact: request.payload.contact
-        });
-
-        user.save(function(err, user){
-          if(err){
-            if(err.code === 11000){ // MongoDB Duplicate Key Error
-              reply(Boom.badRequest('User already exists'));
-            }
-            else if(err.name == 'ValidationError') { // Note: Mongoose errors don't have error codes
-              reply(Boom.badRequest('Missing Username or Password'));
-            }
-            else {
-              throw err;
-            }
-          }
-          else {
-            reply(user);
-          }
-        });
-
-      }
+      handler: userController.create
     },
     {
       method: 'POST',
@@ -45,30 +19,7 @@ module.exports = function(){
     {
       method: 'GET',
       path: '/users/{id}',
-      handler: function (request, reply) {
-
-        User.findById(request.params.id, function (err, user) {
-
-          if (err){
-            if(err.name == 'CastError'){ // Note: Mongoose errors don't have error codes
-              reply(Boom.badRequest('Invalid User Id'));
-            }
-            else {
-              throw err;
-            }
-          }
-          else {
-            if(user){
-              reply(user);
-            }
-            else {
-              reply(Boom.notFound());
-            }
-          }
-
-        });
-
-      }
+      handler: userController.read
     },
     {
       method: 'DELETE',
